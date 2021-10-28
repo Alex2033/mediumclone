b947e98ab9fa2c6f77d9c5fe7af6a9fc4408fb28
Alexandr <XoxlofAlexandr@yandex.ru>
2021-10-28 17:03:02 +0300

    апдейт до 11 версии

--- src/app/shared/modules/backendErrorMessages/backend-error-messages.component.spec.ts
+++ src/app/shared/modules/backendErrorMessages/backend-error-messages.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { BackendErrorMessagesComponent } from './backend-error-messages.component';
 
@@ -6,7 +6,7 @@ describe('BackendErrorMessagesComponent', () => {
   let component: BackendErrorMessagesComponent;
   let fixture: ComponentFixture<BackendErrorMessagesComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ BackendErrorMessagesComponent ]
     })
